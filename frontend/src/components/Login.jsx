676cd68eb65fe3d937e546690ff2064f5751ef66
Kunal Nailwal <102874692+getGlitch@users.noreply.github.com>
2025-05-21 23:43:52 +0530

    Update Login.jsx

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -58,7 +58,7 @@ const handleChange=(e)=>{
   return (
     <div className="flex items-center justify-center min-h-screen bg-gray-100">
       <div className="w-full max-w-md p-8 space-y-6 bg-white rounded-lg shadow-lg">
-        <h2 className="text-3xl font-semibold text-center text-gray-700">Login</h2>
+        <h2 className="text-3xl font-semibold text-center text-gray-700">Login Here ...</h2>
 
         <form onSubmit={handleSubmit} className="mt-4 space-y-4">
          
